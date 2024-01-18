4d9e5cf72b9b34605da7e49f85960aba7de53349
Santillan-Reyna-Ariel-Angel <santillanreynaarielangel@gmail.com>
2024-01-17 20:00:19 -0400

    Desmotrar que todo en python es un objeto

---
+++ hola_mundo.py
@@ -0,0 +1,15 @@
+# import pandas
+
+# print("Hola mundo")
+
+# import this
+
+# CUALQUIER COSA EN PYTHON ES IN OBJETO:
+edad=60
+print(type(edad))
+nombre="Juan"
+print(type(nombre))
+
+print(id(edad)) # id() nos da el id de la variable
+print(repr(edad)) # repr() nos da el valor de la variable
+
